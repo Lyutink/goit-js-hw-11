cc7e4a9d57424ed5fec8a9ee68923c8839a25bd7
Lyutink <krivenko.lyuda@gmail.com>
2021-11-19 16:42:34 +0300

    up

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,24 +1,9 @@
-const BASE_URL = "https://pixabay.com/api/";
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
 const API_Key = "24366692-ce9347f3f27462bce5924cca4";
-// const options = {
-//     headers: {
-//     key: API_Key,
-//     //q: - термин для поиска. То, что будет вводить пользователь.
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: 'true',
-// }
 
 export function fetchImages(requestInInput) {
-
-    return fetch(`${BASE_URL}?key=${API_Key}&q=${requestInInput}&image_type=photo&orientation=horizontal&safesearch=true`)
-        .then(respons => {
-            if (!respons.ok) {
-            throw Error(respons.statusText);
-        }
-        return console.log(respons.json());
-        })
-        // .catch(() => {
-        //     return console.log(respons);
-        // })
+    return axios.get(`?key=${API_Key}&q=${requestInInput}&image_type=photo&orientation=horizontal&safesearch=true$page=1$per_page=1-40`);
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ let requestFromUser = '';
 
 const searchFormRef = document.querySelector('.search-form');
 const inputRef = document.querySelector('.search-form > input');
+const galleryRef = document.querySelector('.gallery');
 
 
 //searchFormRef.addEventListener('submit', debounce(onRequestFromUser, DEBOUNCE_DELAY));
@@ -20,6 +21,36 @@ function onRequestFromUser(event) {
     console.log(requestFromUser);
     //requestFromUser = requestFromUser.trim();
     console.log("будем вызывать  fetchImages");
-    HTTPServise.fetchImages(requestFromUser);
+    HTTPServise.fetchImages(requestFromUser)
+        .then(response => renderMarkupCard(response.data))
+        .catch(error => console.log("AAAAAAAAAA", error));
 }
+
+// async function renderMarkupCard(response) {
+//     console.log(response.hits);
+//     markup = await response.map(result => result.name);
+//     console.log(markup);
+//}
  
+function renderMarkupCard(result) {
+    //console.log(result.largeImageURL);
+    const markup = result.hits.map((res) => `<div class="photo-card">
+        <img src="${res.previewURL}" alt="" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes: ${res.likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views: ${res.views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments: ${res.comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads: ${res.downloads}</b>
+          </p>
+        </div>
+      </div>`).join('');
+    galleryRef.innerHTML = markup;
+
+}
