0d12e64861df5911adf448c8c727717e24d3c64b
Lyutink <krivenko.lyuda@gmail.com>
2021-11-23 17:30:21 +0300

    up

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ let requestFromUser = '';
 
 const searchFormRef = document.querySelector('.search-form');
 const inputRef = document.querySelector('.search-form > input');
-const galleryRef = document.querySelector('.gallery');
+const galleryRef = document.querySelector('.gallery-list');
 
 
 //searchFormRef.addEventListener('submit', debounce(onRequestFromUser, DEBOUNCE_DELAY));
@@ -31,9 +31,7 @@ function onRequestFromUser(event) {
             console.log("totalHits", response.data.totalHits)
             if (!response.data.totalHits) {
                     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.',
-                    {
-                        timeout: 4000,
-                        });
+                    {timeout: 4000,});
                 clearFoo();
                 return;
             }
@@ -51,7 +49,7 @@ function renderMarkupCard(result) {
     //console.log(result.largeImageURL);
    // <a class="gallery-item" href="${res.largeImageURL}"></a>
     const markup = result.hits.map((res) => `
-    <div class="photo-card">
+    <li class="photo-card">
         <img class="photo-img" src="${res.webformatURL}" alt="${res.tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
@@ -67,7 +65,7 @@ function renderMarkupCard(result) {
             <b>Downloads: ${res.downloads}</b>
           </p>
         </div>
-      </div>
+      </li>
       `).join('');
     galleryRef.innerHTML = markup;
 
